ebb03f28f54352361034834f7df955dd6fe2b96b
Obakeng <obakeng@staffdeals.co.za>
2021-01-21 10:08:49 +0200

    Removed old Manufacturer form and Category form. Also removed frm tag off index.html

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm, Textarea, TextInput, Select
-from .models import Product, Manufacturer, Category
+from .models import Product
 from django.utils.translation import gettext_lazy as _
 
 class ProductForm(ModelForm):
@@ -7,27 +7,10 @@ class ProductForm(ModelForm):
         model = Product
         fields = '__all__'
         widgets = {
-            'product_condition':Select(attrs={'class': 'form-control', 'title': 'Product Condition'}),
-            'product_name': TextInput(attrs={'class': 'form-control'}),
-            'product_expected_price': TextInput(attrs={'class': 'form-control', 'placeholder': 'Expected price'}),
-            'product_year': TextInput(attrs={'class': 'form-control'}),
-            'product_model': TextInput(attrs={'class': 'form-control'}),
-            'product_type': TextInput(attrs={'class': 'form-control'}),
-            'product_variant': Select(attrs={'class': 'form-control'}),
-        }
-
-class ManufacturerForm(ModelForm):
-    class Meta:
-        model = Manufacturer
-        fields = ['manufacturer_name']
-        widgets = {
-            'manufacturer_name': Select(attrs={'class': 'form-control'}),
-        }
-
-class CategoryForm(ModelForm):
-    class Meta:
-        model = Category
-        fields = ['category_name']
-        widgets = {
-            'category_name': Select(attrs={'class': 'form-control'})
+            'brand': Select(attrs={'class': 'form-control', 'title': 'Product Condition'}),
+            'model': Select(attrs={'class': 'form-control'}),
+            'category': Select(attrs={'class': 'form-control', 'placeholder': 'Expected price'}),
+            'variant': Select(attrs={'class': 'form-control'}),
+            'condition': Select(attrs={'class': 'form-control'}),
+            'accessory': TextInput(attrs={'class': 'form-control'}),
         }
